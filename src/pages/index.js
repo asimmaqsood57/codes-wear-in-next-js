d764ca0482586b2d3d65afb98819839cdbd2b6b7
M.Asim Maqsood <70227239+asimmaqsood57@users.noreply.github.com>
2023-04-06 12:58:18 +0500

    working on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,53 @@
 import React from "react";
 import Head from "next/head";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import heroImg from "../../public/hero-img.jpg";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>CodesWear.com - Wear the codes</title>
         <link rel="icon" href="/favicon.io" />
       </Head>
-      hello
-      <div className="mx-4 ">hello world</div>
-    </div>
+      <Navbar />
+
+      <div>
+        <section class="text-gray-600 body-font">
+          <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                CodesWear.com
+              </h1>
+              <p class="mb-8 leading-relaxed">
+                Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+                air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+                park mlkshk tote bag selvage hot chicken authentic tumeric
+                truffaut hexagon try-hard chambray.
+              </p>
+              <div class="flex justify-center">
+                <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Button
+                </button>
+                <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                  Button
+                </button>
+              </div>
+            </div>
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img
+                class="object-cover object-center rounded"
+                alt="hero"
+                src={heroImg.src}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <Footer />
+    </>
   );
 };
 
