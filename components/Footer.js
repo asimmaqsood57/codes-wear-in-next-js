dbc2c6ef85342dd2412eb9e21946b32e8f2933b3
M.Asim Maqsood <70227239+asimmaqsood57@users.noreply.github.com>
2023-04-06 18:14:11 +0500

    navbar is almost done

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -7,7 +8,9 @@ const Footer = () => {
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <img src="/logo.png" alt="" />
+              <Link href="/">
+                <img src="/logo.png" alt="" />
+              </Link>
             </a>
             <p className="mt-2 text-sm text-gray-500">
               Air plant banjo lyft occupy retro adaptogen indego
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,37 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2">
+        <div className="logo mx-5">
+          <Image width={200} height={50} src="/logo.png" alt="" />
+        </div>
+        <div className="nav">
+          <ul className="flex space-x-2 font-bold">
+            <Link href="/tshirts">
+              <li>T-Shirts</li>
+            </Link>
+            <Link href="/hoodies">
+              <li>Hoodies</li>
+            </Link>
+            <Link href="/mugs">
+              <li>Mugs</li>
+            </Link>
+            <Link href="/">
+              <li>Stickers</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="cart absolute right-2">
+          <AiOutlineShoppingCart className="text-3xl" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
